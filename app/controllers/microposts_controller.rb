9ebcfca2e96c5bba1a1eeeb2122ec3d23b3078a5
tw94sh <dsph2011@gmail.com>
2023-07-31 16:51:42 +0000

    Finish toy app

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,71 +1,70 @@
 class MicropostsController < ApplicationController
-    before_action :set_user, only: %i[ show edit update destroy ]
-  
-    # GET /users or /users.json
-    def index
-      @users = User.all
-    end
-  
-    # GET /users/1 or /users/1.json
-    def show
-    end
-  
-    # GET /users/new
-    def new
-      @user = User.new
-    end
-  
-    # GET /users/1/edit
-    def edit
-    end
-  
-    # POST /users or /users.json
-    def create
-      @user = User.new(user_params)
-  
-      respond_to do |format|
-        if @user.save
-          format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-          format.json { render :show, status: :created, location: @user }
-        else
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    # PATCH/PUT /users/1 or /users/1.json
-    def update
-      respond_to do |format|
-        if @user.update(user_params)
-          format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-          format.json { render :show, status: :ok, location: @user }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    # DELETE /users/1 or /users/1.json
-    def destroy
-      @user.destroy
-  
-      respond_to do |format|
-        format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-        format.json { head :no_content }
-      end
-    end
-  
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_user
-        @user = User.find(params[:id])
-      end
-  
-      # Only allow a list of trusted parameters through.
-      def user_params
-        params.require(:user).permit(:name, :email)
-      end
+  before_action :set_micropost, only: %i[ show edit update destroy ]
+
+  # GET /microposts or /microposts.json
+  def index
+    @microposts = Micropost.all
   end
-  
+
+  # GET /microposts/1 or /microposts/1.json
+  def show
+  end
+
+  # GET /microposts/new
+  def new
+    @micropost = Micropost.new
+  end
+
+  # GET /microposts/1/edit
+  def edit
+  end
+
+  # POST /microposts or /microposts.json
+  def create
+    @micropost = Micropost.new(micropost_params)
+
+    respond_to do |format|
+      if @micropost.save
+        format.html { redirect_to micropost_url(@micropost), notice: "Micropost was successfully created." }
+        format.json { render :show, status: :created, location: @micropost }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /microposts/1 or /microposts/1.json
+  def update
+    respond_to do |format|
+      if @micropost.update(micropost_params)
+        format.html { redirect_to micropost_url(@micropost), notice: "Micropost was successfully updated." }
+        format.json { render :show, status: :ok, location: @micropost }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /microposts/1 or /microposts/1.json
+  def destroy
+    @micropost.destroy
+
+    respond_to do |format|
+      format.html { redirect_to microposts_url, notice: "Micropost was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_micropost
+      @micropost = Micropost.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def micropost_params
+      params.require(:micropost).permit(:content, :user_id)
+    end
+end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,2 @@
 class Micropost < ApplicationRecord
-    belongs_to :user
-    validates :content, length: { maximum: 140 }, presence: true
 end
